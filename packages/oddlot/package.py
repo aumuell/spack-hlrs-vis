ee55cb2b7b969f6c5d6709d7914c1c918bb4981a
Martin Aumüller <aumuell@reserv.at>
2021-10-03 03:21:15 +0200

    oddlot: add package for OddLOT

---
+++ packages/oddlot/package.py
@@ -0,0 +1,35 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+from .opencover import Opencover
+from .opencover import HlrsCovisePackage
+
+
+class Oddlot(HlrsCovisePackage):
+    """OddLOT: The OpenDRIVE Designer for Lanes, Objects and Tracks."""
+
+    homepage = "https://www.hlrs.de/oddlot"
+
+    depends_on('proj@:7.99')
+    depends_on('xerces-c')
+    depends_on('libtiff')
+    depends_on('libpng')
+    depends_on('libjpeg-turbo')
+    depends_on('boost')
+    depends_on('opencrg', type='build') # static library only
+    depends_on('eigen')
+    depends_on('glew')
+    depends_on('qt+opengl@5.0:5.99')
+
+    def cmake_args(self):
+        """Populate cmake arguments for OddLOT."""
+
+        args = HlrsCovisePackage.cmake_covise_args(self)
+
+        args.append('-DCOVISE_BUILD_ONLY_ODDLOT=ON')
+
+        return self.cmake_disable_implicit_deps(args)
