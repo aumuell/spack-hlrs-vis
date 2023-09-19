a077bc37b54c74fc01f40bc7b80d7b854148a2ef
Martin Aumüller <aumuell@reserv.at>
2023-09-20 00:52:17 +0200

    osgcal: use checksummable archives

--- packages/osgcal/package.py
+++ packages/osgcal/package.py
@@ -10,14 +10,15 @@ class Osgcal(CMakePackage):
     """Cal3D adapter for OpenSceneGraph, imported from https://sourceforge.net/p/osgcal/code"""
 
     homepage = "http://osgcal.sourceforge.net"
-    git      = "https://github.com/hlrs-vis/osgcal.git"
+    url = "https://github.com/hlrs-vis/osgcal/archive/refs/tags/2022.5.tar.gz"
+    git = "https://github.com/hlrs-vis/osgcal.git"
 
     maintainers = ['aumuell']
 
     version('master', tag='master')
-    version('2022.5', commit='66f92936b4783627b71ca640e38cc3789d6dc479')
-    version('2021.10', commit='ab2bbb1579479923acd6b96a07101bf07cec3038')
-    version('2018.5.1', commit='fbefbd54bb4ec822bb192073f93e904b2645a162')
+    version("2022.5", sha256="ae35a1b84ed89f5a57e3dc294f8f737056df7cff95fc3cf0cc935765c7f059b5")
+    version("2021.10", sha256="0a4818e58e5e9cb4e44bad2f21c93e8a406e3d185b431cba3d912fb1a5d01471")
+    version("2018.5.1", sha256="a819efdbcee6a46c1d831e33b928abf1cb734c9c82b473359d43ea8c7241ded8")
 
     depends_on('openscenegraph')
     depends_on('cal3d')
