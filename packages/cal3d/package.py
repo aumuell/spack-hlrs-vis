1054ff29158306189a5170c52f87b77ee66f208b
Martin Aumüller <aumuell@reserv.at>
2023-10-18 19:35:10 +0200

    cal3d: build from archive instead of git repo

--- packages/cal3d/package.py
+++ packages/cal3d/package.py
@@ -9,17 +9,14 @@ class Cal3d(AutotoolsPackage):
     """Skeletal 3D character animation library written in C++"""
 
     homepage = 'http://cal3d.sourceforge.net/docs/api/html/cal3dfaq.html'
-    git      = "https://github.com/hlrs-vis/cal3d.git"
+    url = "https://github.com/hlrs-vis/cal3d/archive/refs/tags/v2021.10.tar.gz"
+    git  = "https://github.com/hlrs-vis/cal3d.git"
 
     maintainers = ['aumuell']
 
     version('master', preferred=False, branch='master')
-    version('2021.10', commit='8c45a264acb881c026c3e3c2e7a07826aa2eaa01')
-    version('2018.5.2', commit='8cbf44f8c20a191b67063cb943420ec55e7125b8')
-
-    #url "https://github.com/hlrs-vis/libe57.git", branch: "main", revision: "6c6b0c8355d870342f1736bfcc3b5299fe012d4c"
-    #license "http://libe57.org/license.html"
-    #head "https://github.com/hlrs-vis/libe57.git", branch: "main"
+    version("2021.10", sha256="f35bb2dd034f254cecf47f66b67bb76aa421180bd662f0258760b9a2875f5a07")
+    version("2018.5.2", sha256="55e61e04d85e4fd8bb608f477558ffcb616a48988f9e45ec244aa2ef59e21732")
 
     depends_on('autoconf', type='build')
     depends_on('automake', type='build')
@@ -37,14 +34,6 @@ class Cal3d(AutotoolsPackage):
         args.append('--disable-dependency-tracking')
         args.append('--disable-silent-rules')
         args.append('--prefix={0}'.format(self.prefix))
-
-        return args
-
-    def cmake_args(self):
-        """Populate cmake arguments for Cal3D."""
-        spec = self.spec
-
-        args = []
 
         return args
 
