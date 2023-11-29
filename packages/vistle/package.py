cfd1b5541e3690e7c1e52d407057fdb7758bc1dd
Martin Aumüller <aumuell@reserv.at>
2023-11-29 10:58:45 +0100

    vistle: depend on netcdf-c that can read our MPAS data

--- packages/vistle/package.py
+++ packages/vistle/package.py
@@ -70,7 +70,7 @@ class Vistle(HlrsCMakePackage):
     depends_on('boost+pic', when='+static')
     depends_on('boost+mpi', when='+boostmpi')
 
-    depends_on('netcdf-c')
+    depends_on('netcdf-c +hdf4') # hdf4 for MPAS
     depends_on('netcdf-cxx4', when='+netcdf')
     depends_on('parallel-netcdf', when='+pnetcdf')
     depends_on('xdmf3', when='+xdmf')
